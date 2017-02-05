3a27dc97bf97552ca2dc4272973588e549dcf1ff
avwo <avwu@qq.com>
2017-02-05 13:54:07 +0800

    test: isInNet

--- test/scripts/isInNet.js
+++ test/scripts/isInNet.js
@@ -1,7 +1,6 @@
 function FindProxyForURL(url, host) {
   if (!isInNet(dnsResolve(host), '192.168.0.0', '255.255.255.0')) {
-    return "DIRECT";
+    return "SOCKS isinnet.com:8080";
   }
-  
-  return "SOCKS isinnet.com:8080";
+  return "DIRECT";
 }
